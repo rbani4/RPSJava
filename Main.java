4659c4482a3da5c3f80ae174c5a86be613474e95
rbani4 <151563112+rbani4@users.noreply.github.com>
2024-01-28 20:54:08 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
-import javax.swing.SwingUtilities;
-
 public class Main {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new RockPaperScissorsGUI());
+        MainMenu i = new MainMenu();
     }
 }
